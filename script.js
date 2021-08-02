7aaae820f051c329384fbeb74585f1f092b8716d
MariiaDenysova <maria.denadel@gmail.com>
2021-08-02 11:45:14 +0200

    add function add Items In Shopping List

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-let shoppingList = [
+let products = [
                     {name: 'Toy', price: 99},
                     {name: 'Chicken', price: 199},
                     {name: 'Rooster', price: 9},
                     {name: 'Cat', price: 19},
 
 ];
-
-
-for (let nameAndPrice of shoppingList) {
-    let tableRow = $('<tr/>', {class: 'table-row'});
-    tableRow.appendTo('#table-body');
-    let tableDataName = $('<td/>', {text: nameAndPrice.name, class: 'name'})
-    let tableDataPrice = $('<td/>', {text: nameAndPrice.price, class: 'price'})
-    tableDataName.appendTo(tableRow);
-    tableDataPrice.appendTo(tableRow);
+function addItemsInShoppingList(products) {
+    for (let nameAndPrice of products) {
+        let tableRow = $('<tr/>', {class: 'table-row'});
+        tableRow.appendTo('#table-body');
+        let tableDataName = $('<td/>', {text: nameAndPrice.name, class: 'name'})
+        let tableDataPrice = $('<td/>', {text: nameAndPrice.price, class: 'price'})
+        tableDataName.appendTo(tableRow);
+        tableDataPrice.appendTo(tableRow);
+    }
 }
+
+addItemsInShoppingList(products)
 
 
 
