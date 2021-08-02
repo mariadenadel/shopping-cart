529b04dfea9cf587254608de2810e778ed4ce8e9
MariiaDenysova <maria.denadel@gmail.com>
2021-08-02 12:12:25 +0200

    add rendet cart items, delete previous shopping list and add current

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-let products = [
-                    {name: 'Toy', price: 99},
-                    {name: 'Chicken', price: 199},
-                    {name: 'Rooster', price: 9},
-                    {name: 'Cat', price: 19},
+let cartItems = [
+    {name: 'Toy', price: 99},
+    {name: 'Chicken', price: 199},
+    {name: 'Rooster', price: 9},
+    {name: 'Cat', price: 19},
+    {name: 'Dog', price: 129},
 
 ];
-function addItemsInShoppingList(products) {
-    for (let nameAndPrice of products) {
+function renderCartItems() {
+    $('#table-body').text('');
+    for (let nameAndPrice of cartItems) {
         let tableRow = $('<tr/>', {class: 'table-row'});
         tableRow.appendTo('#table-body');
         let tableDataName = $('<td/>', {text: nameAndPrice.name, class: 'name'})
         let tableDataPrice = $('<td/>', {text: nameAndPrice.price, class: 'price'})
         tableDataName.appendTo(tableRow);
         tableDataPrice.appendTo(tableRow);
+
     }
 }
 
-addItemsInShoppingList(products)
+renderCartItems()
+
+function addCartItem(product) {
+    products.push(product)
+    renderCartItems()
+}
+
+function removeCartItem(product) {
+    products.delete() // magic
+    renderCartItems()
+}
+
+
 
 
 
